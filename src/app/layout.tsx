ef38ae27bd8237bdc125f13d64e160808371b671
HELAL15 <1ahmedhelal1@gmail.com>
2025-08-05 16:59:58 +0300

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { ReactNode } from 'react';
 
 import type { Metadata } from 'next';
 
+// import { Jost, Marcellus } from 'next/font/google';
+
 import AppProvider from '@/providers/AppProvider';
 import { Analytics } from '@vercel/analytics/next';
 
@@ -13,13 +15,26 @@ export const metadata: Metadata = {
     description: 'Kaira Fashion-Store Ecommerce Website'
 };
 
+// const jost = Jost({
+//     subsets: ['latin'],
+//     variable: '--font-jost',
+//     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+//     display: 'swap'
+// });
+
+// const marcellus = Marcellus({
+//     weight: '400',
+//     subsets: ['latin'],
+//     variable: '--font-marcellus'
+// });
+
 const Layout = async ({ children }: Readonly<{ children: ReactNode }>) => {
     const locale = await getLocale();
     const dir = locale === 'ar' ? 'rtl' : 'ltr';
 
     return (
         <html suppressHydrationWarning lang={locale} dir={dir}>
-            <body className='bg-background !text-foreground relative overflow-x-clip antialiased'>
+            <body className={`bg-background !text-foreground relative overflow-x-clip antialiased`}>
                 <AppProvider>
                     {children}
                     <Analytics />
