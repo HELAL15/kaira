ffe5f2c237cbb3fa9ef50cce61fb4c1b9ad4f59c
HELAL15 <ah4459577@gmail.com>
2025-08-07 22:44:30 +0300

    add google analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,37 @@
 import type { ReactNode } from 'react';
 
 import type { Metadata } from 'next';
-
-// import { Jost, Marcellus } from 'next/font/google';
+import Head from 'next/head';
+import Script from 'next/script';
 
 import AppProvider from '@/providers/AppProvider';
 import { Analytics } from '@vercel/analytics/next';
 
 import 'aos/dist/aos.css';
 
-// import { getLocale } from 'next-intl/server';
-
 export const metadata: Metadata = {
     title: 'Kaira - home',
     description: 'Kaira Fashion-Store Ecommerce Website'
 };
 
-// const jost = Jost({
-//     subsets: ['latin'],
-//     variable: '--font-jost',
-//     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-//     display: 'swap'
-// });
-
-// const marcellus = Marcellus({
-//     weight: '400',
-//     subsets: ['latin'],
-//     variable: '--font-marcellus'
-// });
-
 const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
-    // const locale = await getLocale();
-    // const dir = locale === 'ar' ? 'rtl' : 'ltr';
-
     return (
         <html suppressHydrationWarning lang={'en'} dir={'ltr'}>
+            <head>
+                <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-S8XQ409H2C' />
+                <Script
+                    id='google-analytics'
+                    strategy='afterInteractive'
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-S8XQ409H2C');
+                        `
+                    }}
+                />
+            </head>
             <body className={`bg-background !text-foreground relative overflow-x-clip antialiased`}>
                 <AppProvider>
                     {children}
