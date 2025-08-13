5c27fad3ea95a5495b5193e0c00c75b5b6f76493
HELAL15 <1ahmedhelal1@gmail.com>
2025-08-13 13:23:48 +0300

    change image sizes

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -16,13 +16,17 @@ const ProductCard = ({ product, delay }: { product?: FieldValues; delay?: number
                 <CardContent className='group relative overflow-hidden p-0'>
                     <Image
                         src={image || product_banner || '/images/cat-item1.jpg'}
-                        alt={'product'}
-                        width={0}
-                        height={0}
+                        alt='product'
+                        width={300}
+                        height={300}
                         loading='lazy'
-                        sizes='100'
+                        decoding='async'
+                        placeholder='blur'
+                        blurDataURL='/images/blur-placeholder.jpg'
+                        sizes='(max-width: 768px) 100vw, 300px'
                         className='h-[250px] w-full object-cover duration-300 group-hover:scale-110 md:h-[390px] lg:h-[400px]'
                     />
+
                     <div className='absolute -bottom-full flex w-full flex-wrap items-center justify-center gap-3 space-y-2 px-3 py-6 text-start text-white duration-300 group-hover:bottom-0'>
                         <Button title='add to cart' className='m-0 px-5 uppercase'>
                             add to cart
